b9da27e23662901cc06c32df49745e6210363714
James Pulec <jpulec@gmail.com>
2025-07-07 18:17:59 -0700

    chore: add node version

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,7 @@
-import { handle } from 'hono/vercel';
-import app from '../src/index';
+import { handle } from "hono/vercel";
+import app from "../src/index";
+
+console.log("Node.js version:", process.version);
 
 export const POST = handle(app);
 export const GET = handle(app);
